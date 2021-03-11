2c18d847de1911b7e6e1ef283a186092afda0d30
Dillon DeGarmo <cddegarmo@gmail.com>
2021-03-11 09:39:11 -0600

    Add method for water volume

--- src/Volumetrics.java
+++ src/Volumetrics.java
@@ -1,8 +1,9 @@
 
 public class Volumetrics {
-    public static final int BBLS = 7_758;  // barrels (42 gallon) per acre-foot
-    public static final int MCF = 43_560;  // cubic feet of gas per acre-foot
+    public static final int BBLS = 7758;  // barrels (42 gallon) per acre-foot
+    public static final int MCF = 43560;  // cubic feet of gas per acre-foot
 
+    // Prohibit instantiation
     private Volumetrics() {}
 
     public static double oilInPlace(double area, int thickness,
@@ -21,6 +22,15 @@ public class Volumetrics {
 
         double value = MCF * area * thickness * gasSaturation * gasSaturation * porosity;
         value /= volumeFactor;
+
+        return Math.round(value * 100.0) / 100.0;
+    }
+
+    // Generally only used to evaluate potential for water disposal, or as an academic exercise
+    public static double waterInPlace(double area, int thickness,
+                                      double porosity) {
+
+        double value = BBLS * area * thickness * porosity;
 
         return Math.round(value * 100.0) / 100.0;
     }
