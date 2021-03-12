c7c03375b8a1a834a6b22a753c836c1e56af0138
Dillon DeGarmo <cddegarmo@gmail.com>
2021-03-12 14:10:12 -0600

    Add class for petrophysical calculations

---
+++ src/Petrophysics.java
@@ -0,0 +1,21 @@
+public class Petrophysics {
+
+    private Petrophysics() {}
+
+    /**
+     * Calculates water saturation of a reservoir via Archie's equation.
+     * @param n   saturation exponent
+     * @param rw  formation water resistivity
+     * @param phi porosity
+     * @param m   cementation exponent
+     * @param rf  formation resistivity
+     * @return double water saturation, in range 0 to 1.
+     */
+    public static double waterSaturation(double n, double rw, double phi,
+                                         double m, double rf) {
+        double value = rw / (Math.pow(phi, m) * rf);
+        value = Math.pow(value, (1 / n));
+
+        return Math.round(value * 100.0) / 100.0;
+    }
+}
