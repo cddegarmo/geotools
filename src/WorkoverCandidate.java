c8d2ed5a004b3d6d2bc3bec915b815020f275ba9
Dillon DeGarmo <cddegarmo@gmail.com>
2021-03-18 14:33:33 -0500

    Create User class to contain client code

--- src/WorkoverCandidate.java
+++ src/WorkoverCandidate.java
@@ -28,7 +28,7 @@ public class WorkoverCandidate {
     private int getAdjacent()    { return adjacentInjectors; }
     private double getPorosity() { return porosity;          }
 
-    private static final Comparator<WorkoverCandidate> COMPLETION_ORDER =
+    public static final Comparator<WorkoverCandidate> COMPLETION_ORDER =
             comparingInt(WorkoverCandidate::getAdjacent)
             .thenComparingInt(WorkoverCandidate::getNetPay)
             .thenComparingDouble(WorkoverCandidate::getPorosity)
@@ -54,33 +54,5 @@ public class WorkoverCandidate {
     @Override
     public String toString() {
         return Integer.toString(wellNumber);
-    }
-
-    // Example client code
-    public static void main(String[] args) {
-        List<WorkoverCandidate> wells = new ArrayList<>();
-
-        WorkoverCandidate one = new WorkoverCandidate(3262, 39, 4, 0.075);
-        WorkoverCandidate two = new WorkoverCandidate(3263, 45, 4, 0.069);
-        WorkoverCandidate three = new WorkoverCandidate(4236, 38, 2, 0.066);
-        WorkoverCandidate four = new WorkoverCandidate(3271, 39, 3, 0.067);
-        WorkoverCandidate five = new WorkoverCandidate(3256, 24, 4, 0.07);
-        WorkoverCandidate six = new WorkoverCandidate(3317, 35, 3, 0.064);
-        WorkoverCandidate seven = new WorkoverCandidate(3264, 33, 2, 0.062);
-        WorkoverCandidate eight = new WorkoverCandidate(3226, 35, 2, 0.07);
-
-        wells.add(one);
-        wells.add(two);
-        wells.add(three);
-        wells.add(four);
-        wells.add(five);
-        wells.add(six);
-        wells.add(seven);
-        wells.add(eight);
-
-        Collections.sort(wells, COMPLETION_ORDER);
-
-        for(WorkoverCandidate well : wells)
-            System.out.println(well.toString());
     }
 }
