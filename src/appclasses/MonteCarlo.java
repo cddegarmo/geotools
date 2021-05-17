7ef2004a0589c03eed325af5e864155fb39c39db
Dillon DeGarmo <cddegarmo@gmail.com>
2021-05-17 10:06:52 -0500

    Add method to 'spin the wheel' with chance of success or failure

--- src/appclasses/MonteCarlo.java
+++ src/appclasses/MonteCarlo.java
@@ -3,21 +3,39 @@ package appclasses;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class MonteCarlo {
-   public static final int getRandInt(int bound) {
+   public static int runSpinner(int chance) {
+      if (chance < 1 || chance > 99)
+         throw new IllegalArgumentException("Chance of success must be between 0 and 100, exclusive.");
+      int probability = ThreadLocalRandom.current().nextInt(1, 100);
+      if (probability <= chance) {
+         System.out.println("Success!");
+         return 1;
+      }
+      else {
+         System.out.println("Failure");
+         return 0;
+      }
+   }
+
+   public static int getRandInt(int bound) {
       return ThreadLocalRandom.current().nextInt(bound);
    }
 
-   public static final int getRandInt(int lower, int bound) {
+   public static int getRandInt(int lower, int bound) {
       return ThreadLocalRandom.current().nextInt(lower, bound);
    }
 
-   public static final double getRandDouble(double bound) {
+   public static double getRandDouble(double bound) {
       return ThreadLocalRandom.current().nextDouble(bound);
    }
 
-   public static final double getRandDouble(double lower, double bound) {
+   public static double getRandDouble(double lower, double bound) {
       return ThreadLocalRandom.current().nextDouble(lower, bound);
    }
 
    private MonteCarlo() {}
+
+   public static void main(String[] args) {
+      runSpinner(90);
+   }
 }
