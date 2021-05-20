9ce3c77096aff566bd3e3f8fd9eb4f5311c8fe4a
Dillon DeGarmo <cddegarmo@gmail.com>
2021-05-20 11:17:17 -0500

    Add new Comparator that ranks wells based on weighted values

--- src/appclasses/WorkoverCandidate.java
+++ src/appclasses/WorkoverCandidate.java
@@ -31,13 +31,69 @@ public class WorkoverCandidate {
     public int getAdjacent()    { return adjacentInjectors; }
     public double getPorosity() { return porosity;          }
 
-    // Comparator serves as main tool for sorting these candidates
-    // Future versions may incorporate various sorting criteria
+    // Comparators serve as main tool for sorting these candidates
+    // Comparator #1
     public static final Comparator<WorkoverCandidate> COMPLETION_ORDER =
             comparingInt(WorkoverCandidate::getAdjacent)
             .thenComparingInt(WorkoverCandidate::getNetPay)
             .thenComparingDouble(WorkoverCandidate::getPorosity)
                     .reversed();
+
+    // Comparator #2
+    public static final Comparator<WorkoverCandidate> WEIGHTED_ORDER =
+            comparingDouble(WorkoverCandidate::getScore)
+            .reversed();
+
+    public final double getScore() {
+        double result = (injRank() * 0.2) +
+                     (porosityRank() * 0.35) +
+                     (netPayRank() * 0.45);
+        return Math.round(result * 100) / 100.0;
+    }
+
+    private int injRank() {
+        return adjacentInjectors;
+    }
+
+    private int porosityRank() {
+        Map<Integer, List<Double>> ranges = new HashMap<>();
+        ranges.put(1, List.of(0.0, 0.05));
+        ranges.put(2, List.of(0.06, 0.1));
+        ranges.put(3, List.of(0.11, 0.15));
+        ranges.put(4, List.of(0.16, 0.2));
+        ranges.put(5, List.of(0.21, 0.3));
+        List<Double> range = ranges.values()
+             .stream()
+             .filter(list -> porosity <= list.get(1) &&
+                  porosity >= list.get(0))
+             .findAny()
+             .orElse(new ArrayList<>());
+        int result = 0;
+        for (Integer score : ranges.keySet())
+            if (ranges.get(score) == range)
+                result = score;
+        return result;
+    }
+
+    private int netPayRank() {
+        Map<Integer, List<Integer>> ranges = new HashMap<>();
+        ranges.put(1, List.of(0, 10));
+        ranges.put(2, List.of(11, 20));
+        ranges.put(3, List.of(21, 30));
+        ranges.put(4, List.of(31, 40));
+        ranges.put(5, List.of(41, 100));
+        List<Integer> range = ranges.values()
+             .stream()
+             .filter(list -> netPay <= list.get(1) &&
+                  netPay >= list.get(0))
+             .findAny()
+             .orElse(new ArrayList<>());
+        int result = 0;
+        for (Integer score : ranges.keySet())
+            if (ranges.get(score) == range)
+                result = score;
+        return result;
+    }
 
     @Override
     public boolean equals(Object o) {
