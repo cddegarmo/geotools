0d22c6e041a9c6fd24d766e82037161d84632871
Dillon DeGarmo <cddegarmo@gmail.com>
2021-08-27 11:28:31 -0500

    Add tests for Volumetrics class

---
+++ src/test/VolumetricsTest.java
@@ -0,0 +1,27 @@
+package test;
+import static appclasses.Volumetrics.*;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class VolumetricsTest {
+   @Test
+   public void testOilInPlace() {
+      assertEquals(18_404_535.45, oilInPlace(240, 87.4, 0.71,
+                                             0.18, 1.13));
+      assertEquals(2_656_723.82, oilInPlace(620, 15.2, 0.43,
+                                            0.12, 1.42));
+   }
+
+   @Test
+   public void testOilInPlaceZeroes() {
+      assertEquals(0, oilInPlace(240, 0, 0.71,
+                                             0.18, 1.13));
+   }
+
+   @Test
+   public void testOilInPlaceNegatives() {
+      assertThrows(IllegalArgumentException.class,
+                   () -> oilInPlace(240, -87.4, 0.71,
+                                             0.18, 1.13));
+   }
+}
