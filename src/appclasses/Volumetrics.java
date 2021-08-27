07a8f48ccafc77c7660999d6aa0df723cafefd8c
Dillon DeGarmo <cddegarmo@gmail.com>
2021-08-27 11:26:09 -0500

    Update volumetric calculations to take all double parameters and also check for negatives

--- src/appclasses/Volumetrics.java
+++ src/appclasses/Volumetrics.java
@@ -5,57 +5,63 @@ import java.util.*;
 
 public class Volumetrics {
     public static final int BBLS = 7758;       // barrels (42 gallon) per acre-foot
-    public static final int MCF = 43560;       // cubic feet of gas per acre-foot
+    public static final int MCF = 43_560;       // cubic feet of gas per acre-foot
     public static final double MILE = 5280.0;  // one mile in feet
 
     // Prohibit instantiation
     private Volumetrics() {}
 
-    public static double oilInPlace(double area, int thickness,
+    public static double oilInPlace(double area, double thickness,
                                         double oilSaturation, double porosity,
                                         double volumeFactor) {
 
         double value = BBLS * area * thickness * oilSaturation * porosity;
+        if (value < 0 || volumeFactor < 0)
+            throw new IllegalArgumentException("Negative parameters are invalid for this calculation");
         value /= volumeFactor;
 
+        // Round off to two decimal places
         return Math.round(value * 100) / 100.0;
     }
 
-    public static double oilInPlaceMC(double areaLow, double areaHigh, int thicknessLow, int thicknessHigh,
+    public static double oilInPlaceMC(double areaLow, double areaHigh, double thicknessLow, double thicknessHigh,
                                       double satLow, double satHigh, double porLow, double porHigh,
                                       double vfLow, double vfHigh) {
         double area = getRandDouble(areaLow, areaHigh);
-        int thickness = getRandInt(thicknessLow, thicknessHigh + 1);
+        double thickness = getRandDouble(thicknessLow, thicknessHigh + 1);
         double saturation = getRandDouble(satLow, satHigh);
         double porosity = getRandDouble(porLow, porHigh);
         double volumeFactor = getRandDouble(vfLow, vfHigh);
         return oilInPlace(area, thickness, saturation, porosity, volumeFactor);
     }
 
-    public static double oilInPlaceMCCast(List<? extends Number> parameters) {
-        double area = getRandDouble((Double) parameters.get(0), (Double) parameters.get(1));
-        int thickness = getRandInt((Integer) parameters.get(2), (Integer) parameters.get(3));
-        double saturation = getRandDouble((Double) parameters.get(4), (Double) parameters.get(5));
-        double porosity = getRandDouble((Double) parameters.get(6), (Double) parameters.get(7));
-        double volumeFactor = getRandDouble((Double) parameters.get(8), (Double) parameters.get(9));
+    public static double oilInPlaceMCCast(List<Double> parameters) {
+        double area = getRandDouble(parameters.get(0), parameters.get(1));
+        double thickness = getRandDouble(parameters.get(2), parameters.get(3));
+        double saturation = getRandDouble(parameters.get(4), parameters.get(5));
+        double porosity = getRandDouble(parameters.get(6), parameters.get(7));
+        double volumeFactor = getRandDouble(parameters.get(8), parameters.get(9));
         return oilInPlace(area, thickness, saturation, porosity, volumeFactor);
     }
 
-    public static double gasInPlace(double area, int thickness,
+    public static double gasInPlace(double area, double thickness,
                                     double gasSaturation, double porosity,
                                     double volumeFactor) {
 
         double value = MCF * area * thickness * gasSaturation * gasSaturation * porosity;
+        if (value < 0 || volumeFactor < 0)
+            throw new IllegalArgumentException("Negative parameters are invalid for this calculation");
         value /= volumeFactor;
 
+        // Round off to two decimal places
         return Math.round(value * 100) / 100.0;
     }
 
-    public static double gasInPlaceMC(double areaLow, double areaHigh, int thicknessLow, int thicknessHigh,
+    public static double gasInPlaceMC(double areaLow, double areaHigh, double thicknessLow, double thicknessHigh,
                                       double satLow, double satHigh, double porLow, double porHigh,
                                       double vfLow, double vfHigh) {
         double area = getRandDouble(areaLow, areaHigh);
-        int thickness = getRandInt(thicknessLow, thicknessHigh + 1);
+        double thickness = getRandDouble(thicknessLow, thicknessHigh + 1);
         double saturation = getRandDouble(satLow, satHigh);
         double porosity = getRandDouble(porLow, porHigh);
         double volumeFactor = getRandDouble(vfLow, vfHigh);
@@ -63,17 +69,20 @@ public class Volumetrics {
     }
 
     // Generally only used to evaluate potential for water disposal, or as an academic exercise
-    public static double waterInPlace(double area, int thickness,
+    public static double waterInPlace(double area, double thickness,
                                       double porosity) {
 
         double value = BBLS * area * thickness * porosity;
+        if (value < 0)
+            throw new IllegalArgumentException("Negative parameters are invalid for this calculation");
 
         return Math.round(value * 100) / 100.0;
     }
 
     // Normalize all production volumes to 1-mile lateral
     private static double getLengthFactor(int lateralLength) {
-         return Math.round(lateralLength * 1_000) / MILE;
+        double result = lateralLength / MILE;
+        return Math.round(result * 1000) / 1000.0;
     }
 
     // Rough calculation of remaining reserves in a conventional well
